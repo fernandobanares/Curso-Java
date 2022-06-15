b4ca4e11d83f7f1c1e46d821b2c4d8cc08d6b3ae
fernando.banares <fbanarescarrasco@gmail.com>
2022-06-14 22:45:08 -0400

    Segundo Desafío

--- desafío.js
+++ desafío.js
@@ -1,16 +1,22 @@
 //Desafío Entregable N°1
 
-function saludar(nombre){
-    let frase = `Hola ${nombre}! ¿Como estas?`;
-    document.write(frase)
-}
+// function saludar(nombre){
+//     let frase = `Hola ${nombre}! ¿Como estas?`;
+//     document.write(frase)
+// }
 
-let nombre = prompt("Ingresa Nombre")
+// let nombre = prompt("Ingresa Nombre")
 
-if (nombre=="fernando"){
-    saludar(nombre) 
-}
-else{
-    alert("adios vaquero/a")
-}
-console.log('Finalizado')
+// if (nombre=="fernando"){
+//     saludar(nombre) 
+// }
+// else{
+//     alert("adios vaquero/a")
+// }
+// console.log('Finalizado')
+
+//Desafío Incorporar Arrays
+
+// let free = false
+
+// const validarCliente = (time)=>
